c64e93b6debeafcfab39e387fb005002b406f441
Hongsuk Ryu <hryu@student.42.us.org>
2021-03-08 20:49:25 +0900

    initial commit frontend and backend

---
+++ src/main/java/com/hryu/timeinvestmenttracker/timeinvestmenttracker/TimeInvestmentTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.hryu.timeinvestmenttracker.timeinvestmenttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeInvestmentTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeInvestmentTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hryu/timeinvestmenttracker/timeinvestmenttracker/TimeInvestmentTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hryu.timeinvestmenttracker.timeinvestmenttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeInvestmentTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
